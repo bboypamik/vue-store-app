047ee3e4d2267a0976bd8075f69222f3909bf83c
Miroslav Pantos <bboypamik@gmail.com>
2021-01-21 20:54:03 +0100

    Customer and Products

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Customer from "../views/Customer.vue";
+import Product from "../views/Customer.vue";
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Customer",
+    component: Customer,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/products",
+    name: "products",
+    component: Product,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Customer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+   <button class="btn btn-primary"> Helloo</button> 
+  </div> 
+</template>
+
+<script>
+export default {
+
+  data(){
+    return{
+      customers: [
+      {
+      id: 1,
+      firstName: "Miro",
+      lastName: 'Pantos',
+      email: 'bboypamik@gmail.com'
+      },
+        {
+          id: 2,
+          firstName: "Bjanka",
+          lastName: 'Miric',
+          email: 'Bjanka@gmil.com'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
